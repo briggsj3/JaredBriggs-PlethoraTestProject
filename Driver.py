3b9c45677fbfeab2065ca9c5b590569fa30def1d
Jared Briggs <briggsj3@gmail.com>
2015-12-21 18:25:46 -0700

    Cleaned up the Driver file and its test cases.  Now all three cases run one after another.

--- Driver.py
+++ Driver.py
@@ -4,23 +4,74 @@
 
 
 from Profile import Profile
+import os
+import json
 import time
 
-# Read the json file:
-jsonfile = open("D:\Desktop Stuff\Applications\Plethora test project\Proj\ExampleFiles\Rectangle.json")
-#jsonfile = open("D:\Desktop Stuff\Applications\Plethora test project\Proj\ExampleFiles\ExtrudeCircularArc.json")
-#jsonfile = open("D:\Desktop Stuff\Applications\Plethora test project\Proj\ExampleFiles\CutCircularArc.json")
-filetxt = jsonfile.read()
-jsonfile.close()
 
-import json
-parsed_json = json.loads(filetxt)
-
-# Create the profile:
-profile = Profile(parsed_json)
-
-time.sleep(30)
-
-
-#def PriceFile(filepath):
+# Call this function to parse a JSON file and run the Profiler.
+#   Accepts:  Path to a JSON-formatted file.
+#   Returns:  The cost of the profile (machine cost + material cost) as string.
+def CalculateProfileCost(json_path):
+    print 'Parsing Profile file:  \"' + json_path + '\"...'
     
+    # Get the contents of the file:
+    try:
+        jsonfile = open(json_path, 'r')
+    except Exception, e:
+        print ' ERROR:  CalculateProfileCost() -> An error was encountered ' + \
+            'while attempting to open the file \"' + \
+            json_path + '\" (error = \"' + str(e) + '\").'
+        return -1
+    
+    try:
+        jsontxt = jsonfile.read()
+    except Exception, e:
+        print ' ERROR:  CalculateProfileCost() -> An error was encountered ' + \
+            'while attempting to read the file \"' + \
+            json_path + '\" (error = \"' + str(e) + '\").'
+        jsontxt = ''
+    finally:
+        jsonfile.close()
+    
+    if jsontxt == '':
+        return -2
+    
+    # Convert the JSON contents of the file into a dict:
+    try:
+        jsondict = json.loads(jsontxt)
+    except Exception, e:
+        print ' ERROR:  CalculateProfileCost() -> An error was encountered ' + \
+            'while attempting to convert JSON file \"' + \
+            json_path + '\" (error = \"' + str(e) + '\").'
+        return -3
+    
+    # Create the profile:
+    try:
+        profile = Profile(jsondict)
+    except Exception, e:
+        print ' ERROR:  CalculateProfileCost() -> An error was encountered ' + \
+            'while attempting to create the Profile (error = \"' + str(e) + '\").'
+        return -3
+    
+    print '...finished.'
+    
+    print 'Material Size Required = ' + str(profile.DimX) + ' x ' + \
+        str(profile.DimY) + ' inches.'
+    
+    # Return a currency-style decimal:
+    return profile.TotalCost
+    
+
+# Get the location of this script:
+testdir = os.path.dirname(os.path.realpath(__file__))
+
+# Run the three given tests:
+print 'Total Profile Cost = $' + str(CalculateProfileCost(testdir + \
+    "/ExampleFiles/Rectangle.json")) + '\n'
+print 'Total Profile Cost = $' + str(CalculateProfileCost(testdir + \
+    "/ExampleFiles/ExtrudeCircularArc.json")) + '\n'
+print 'Total Profile Cost = $' + str(CalculateProfileCost(testdir + \
+    "/ExampleFiles/CutCircularArc.json")) + '\n'
+
+time.sleep(50)
